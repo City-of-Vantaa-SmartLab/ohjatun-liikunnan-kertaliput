6855d1667903e30de6098234058e930aad65f458
Thanh Nguyen <thanh.nguyen@gofore.com>
2018-05-09 11:24:37 +0300

    Set up local hot reloading development with docker

--- build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd backend/
-docker build . -t vantaa-exercise-reservations-backend:latest
-cd ..
-docker build . -t vantaa-exercise-reservations:latest
--- deploy-to-production.sh
+++ deploy-to-production.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-./build-docker.sh
+cd backend/
+docker build . -t vantaa-exercise-reservations-backend:latest
+cd ..
+docker build . -t vantaa-exercise-reservations:latest
 docker tag vantaa-exercise-reservations:latest registry.heroku.com/vantaa-silverfox/web:latest
 heroku container:login
 docker push registry.heroku.com/vantaa-silverfox/web
