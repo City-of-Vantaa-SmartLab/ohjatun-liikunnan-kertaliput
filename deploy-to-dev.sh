e05aa7525dbf0372b1468f31b835cc4cc30d44f7
Thanh Nguyen <thanh.nguyen@gofore.com>
2018-05-09 12:40:10 +0300

    Fix typo

--- deploy-to-dev.sh
+++ deploy-to-dev.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-cd backend/
-docker build . -t vantaa-exercise-reservations-backend:latest
-cd ..
-docker build . -t vantaa-exercise-reservations:latest
 docker tag vantaa-exercise-reservations:latest registry.heroku.com/vantaa-silverfox/web:latest
 heroku container:login
 docker push registry.heroku.com/vantaa-silverfox/web
